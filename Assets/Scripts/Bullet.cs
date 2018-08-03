c0ff671801e1a1811dbb5e4440933e0b61b9c095
JjmarX <32579407+JjmarX@users.noreply.github.com>
2018-08-03 17:14:49 +0300

    2DplatfInDevelp

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private float speed = 10.0F;
+    private SpriteRenderer sprite;
+    private Vector3 direction;
+    public Vector3 Direction { set { direction = value; } }
+
+    private void Awake()
+    {
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+	private void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+}
